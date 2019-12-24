1bd3026f9cf299b5fb2bf4a1e24f769a6f7a5797
yarallira555 <yarallira555@outlook.com.br>
2019-12-24 18:21:52 -0300

    Aplicativo de consultar Cep

---
+++ App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App01_ConsultarCEP.Serviço.Modelo;
+using App01_ConsultarCEP.Serviço;
+
+
+namespace App01_ConsultarCEP
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTÃO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args) {
+
+            string cep = CEP.Text.Trim();
+            Endereço end = ViaCepServiço.BuscarEnderecoViaCep(cep);
+
+            
+
+            RESULTADO.Text = "Endereço: " 
+                + "Cep: " + end.cep +"\n" 
+                + "Rua: " + end.logradouro + "\n" 
+                + "Bairro: " + end.bairro + "\n"
+                + "Cidade: " + end.localidade + "\n"
+                + "Estado: " + end.uf + "\n"
+                + " * YARA MÁRCIA LIRA * "
+                //+ "Rua: " + end.complemento + "\n"
+                //+ "Rua: " + end.complemento + "\n"
+                //+ "Rua: " + end.complemento + "\n"
+                ;
+
+        }
+    }
+}
---
+++ App01_ConsultarCEP/App01_ConsultarCEP/App01_ConsultarCEP/Serviço/ViaCepServiço.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App01_ConsultarCEP.Serviço.Modelo;
+using Newtonsoft.Json;
+
+namespace App01_ConsultarCEP.Serviço
+{
+    public class ViaCepServiço
+    {
+        private static string EnderecoURL = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereço BuscarEnderecoViaCep(string cep)
+        {
+
+            string NovoEnderecoURL = string.Format(EnderecoURL, cep);
+
+            WebClient wc = new WebClient();
+
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereço ende = JsonConvert.DeserializeObject<Endereço>(Conteudo);
+
+
+            return ende;
+        }
+
+    }
+}
